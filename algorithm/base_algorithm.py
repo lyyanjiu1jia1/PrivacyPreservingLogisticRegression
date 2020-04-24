bb414e6233dd1551cfe05f1cf67b5ca95e08df0e
clarkieliu <clarkieliu@tencent.com>
2020-04-24 18:08:22 +0800

    init

---
+++ algorithm/base_algorithm.py
@@ -0,0 +1,10 @@
+class PrivacyPreservingLogistic(object):
+    def __init__(self):
+        pass
+
+    def _gen_A(self):
+        """
+        ring graph Laplacian
+        :return:
+        """
+        pass
